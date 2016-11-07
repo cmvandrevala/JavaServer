b7d76ce195fa5506afec418cadeef7298a7a0d97
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2016-11-07 05:57:51 -0600

    Add space to format timestamp

--- src/test/java/LoggerTest.java
+++ src/test/java/LoggerTest.java
@@ -19,37 +19,37 @@ public class LoggerTest {
 
     @Test
     public void serverHasBeenStartedMessage() throws Exception {
-        String expectedResponse = "[ " + date + "] The server has been started on 127.168.241.121:5880";
+        String expectedResponse = "[ " + date + " ] The server has been started on 127.168.241.121:5880";
         assertEquals(expectedResponse, logger.serverHasBeenStartedMessage(date, "127.168.241.121", 5880));
     }
 
     @Test
     public void serverHasBeenStoppedMessage() throws Exception {
-        String expectedResponse = "[ " + date + "] The server running on 127.168.241.121:5880 has been stopped";
+        String expectedResponse = "[ " + date + " ] The server running on 127.168.241.121:5880 has been stopped";
         assertEquals(expectedResponse, logger.serverHasBeenStoppedMessage(date, "127.168.241.121", 5880));
     }
 
     @Test
     public void clientHasConnectedMessage() throws Exception {
-        String expectedResponse = "[ " + date + "] The client has connected on 127.168.241.121:5880";
+        String expectedResponse = "[ " + date + " ] The client has connected on 127.168.241.121:5880";
         assertEquals(expectedResponse, logger.clientHasConnectedMessage(date, "127.168.241.121:5880"));
     }
 
     @Test
     public void clientHasDisconnectedMessage() throws Exception {
-        String expectedResponse = "[ " + date + "] The client at 127.168.241.121:5880 has disconnected";
+        String expectedResponse = "[ " + date + " ] The client at 127.168.241.121:5880 has disconnected";
         assertEquals(expectedResponse, logger.clientHasDisconnectedMessage(date, "127.168.241.121:5880"));
     }
 
     @Test
     public void resourceRequestedMessage() throws Exception {
-        String expectedResponse = "[ " + date + "] The client sent a GET request to /foo";
+        String expectedResponse = "[ " + date + " ] The client sent a GET request to /foo";
         assertEquals(expectedResponse, logger.resourceRequestedMessage(date, "GET", "/foo"));
     }
 
     @Test
     public void resourceDeliveredMessage() throws Exception {
-        String expectedResponse = "[ " + date + "] The server replied to the GET request to /foo with a status code of 200";
+        String expectedResponse = "[ " + date + " ] The server replied to the GET request to /foo with a status code of 200";
         assertEquals(expectedResponse, logger.resourceDeliveredMessage(date, "GET", "/foo", 200));
     }
 
