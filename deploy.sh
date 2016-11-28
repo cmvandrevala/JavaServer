1d37eca0b8336f78d937229fa20e336971315b5f
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2016-11-28 11:30:03 -0600

    Update deploy script to kill old server process

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
-ssh ec2-user@ec2-52-15-103-218.us-east-2.compute.amazonaws.com '(cd JavaServer && git pull)'
+ssh ec2-user@ec2-52-15-103-218.us-east-2.compute.amazonaws.com "pkill -f 'java -jar'"
+ssh ec2-user@ec2-52-15-103-218.us-east-2.compute.amazonaws.com '(cd JavaServer && git fetch --all)'
+ssh ec2-user@ec2-52-15-103-218.us-east-2.compute.amazonaws.com '(cd JavaServer && git reset --hard origin/master)'
 ssh ec2-user@ec2-52-15-103-218.us-east-2.compute.amazonaws.com 'rm -rf JavaServer/src/test'
 ssh ec2-user@ec2-52-15-103-218.us-east-2.compute.amazonaws.com '(cd JavaServer && mvn clean)'
 ssh ec2-user@ec2-52-15-103-218.us-east-2.compute.amazonaws.com '(cd JavaServer && mvn package)'
