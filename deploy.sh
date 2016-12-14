55a23923c8bb24b5bc3a692705bc15d97e78c434
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2016-12-14 12:45:46 -0600

    Downgrade deploy file to simply copy a local version of the jar file to the server

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-ssh ec2-user@52.15.176.188 "pkill -f 'java -jar'"
-ssh ec2-user@52.15.176.188 '(cd JavaServer && git fetch --all)'
-ssh ec2-user@52.15.176.188 '(cd JavaServer && git reset --hard origin/master)'
-ssh ec2-user@52.15.176.188 'rm -rf JavaServer/src/test'
-ssh ec2-user@52.15.176.188 '(cd JavaServer && mvn clean)'
-ssh ec2-user@52.15.176.188 '(cd JavaServer && mvn package)'
-ssh -n -f ec2-user@52.15.176.188 "sh -c 'cd JavaServer; nohup java -jar out/artifacts/JavaServer_jar/JavaServer.jar > /dev/null 2>&1 &'"
+ssh ec2-user@52.15.206.185 "pkill -f 'java -jar'"
+ssh ec2-user@52.15.206.185 "rm -f ~/JavaServer.jar server.log"
+scp out/artifacts/JavaServer_jar/JavaServer.jar ec2-user@52.15.206.185:~
+ssh -n -f ec2-user@52.15.206.185 "sh -c 'nohup java -jar JavaServer.jar > /dev/null 2>&1 &'"
