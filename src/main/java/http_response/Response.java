723f08ecb4ea9b6c3bea5d0cc97d8885136ad173
Cyrus Vandrevala <cyrus.vandrevala@gmail.com>
2016-12-14 15:35:03 -0600

    Add Response abstraction

---
+++ src/main/java/http_response/Response.java
@@ -0,0 +1,44 @@
+package http_response;
+
+import java.util.Hashtable;
+
+public class Response {
+
+    private final Hashtable<String, String> response;
+
+    public Response(Hashtable<String,String> params) {
+        this.response = emptyResponse();
+        this.response.putAll(params);
+    }
+
+    public String protocol() {
+        return response.get("Protocol");
+    }
+
+    public String statusCode() {
+        return response.get("Status-Code");
+    }
+
+    public String statusMessage() {
+        return response.get("Status-Message");
+    }
+
+    public String contentType() {
+        return response.get("Content-Type");
+    }
+
+    public String connection() {
+        return response.get("Connection");
+    }
+
+    private Hashtable<String, String> emptyResponse() {
+        Hashtable<String, String> emptyHashtable = new Hashtable<>();
+        emptyHashtable.put("Protocol", "");
+        emptyHashtable.put("Status-Code", "");
+        emptyHashtable.put("Status-Message", "");
+        emptyHashtable.put("Content-Type", "");
+        emptyHashtable.put("Connection", "");
+        return emptyHashtable;
+    }
+
+}
---
+++ src/test/java/http_response/ResponseTest.java
@@ -0,0 +1,78 @@
+package http_response;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Hashtable;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class ResponseTest {
+
+    private Response response;
+    private Response emptyResponse;
+    private Hashtable<String,String> params = new Hashtable<>();
+
+    @Before
+    public void setup() {
+        params.put("Protocol", "HTTP/1.1");
+        params.put("Status-Code", "200");
+        params.put("Status-Message", "OK");
+        params.put("Content-Type", "text/html");
+        params.put("Connection", "close");
+        response = new Response(params);
+        emptyResponse = new Response(new Hashtable<>());
+    }
+
+    @Test
+    public void protocolIsEmptyByDefault() {
+        assertEquals("", emptyResponse.protocol());
+    }
+
+    @Test
+    public void itHasAProtocol() {
+        assertEquals("HTTP/1.1", response.protocol());
+    }
+
+    @Test
+    public void statusCodeIsEmptyByDefault() {
+        assertEquals("", emptyResponse.statusCode());
+    }
+
+    @Test
+    public void itHasAStatusCode() {
+        assertEquals("200", response.statusCode());
+    }
+
+    @Test
+    public void statusMessageIsEmptyByDefault() {
+        assertEquals("", emptyResponse.statusMessage());
+    }
+
+    @Test
+    public void itHasAStatusMessage() {
+        assertEquals("OK", response.statusMessage());
+    }
+
+    @Test
+    public void contentTypeIsEmptyByDefault() {
+        assertEquals("", emptyResponse.contentType());
+    }
+
+    @Test
+    public void itHasAContentType() {
+        assertEquals("text/html", response.contentType());
+    }
+
+    @Test
+    public void connectionIsEmptyByDefault() {
+        assertEquals("", emptyResponse.connection());
+    }
+
+    @Test
+    public void itHasAConnection() {
+        assertEquals("close", response.connection());
+    }
+
+
+}
